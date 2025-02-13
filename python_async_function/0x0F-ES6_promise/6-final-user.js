36d2b36d9c2e0e5b65f8d1b669d023da2ab1e505
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2025-02-13 14:22:56 +0100

    caching

--- python_async_function/0x0F-ES6_promise/6-final-user.js
+++
@@ -1,9 +0,0 @@
-// Handle multiple promises
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
-
-export default handleProfileSignup;
